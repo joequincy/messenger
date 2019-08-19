4212e7a371d958c8a48f505492c95f8aa521e385
Jon Peterson <github@joequincy.dev>
2019-08-19 09:39:49 -0600

    Add test for announcing room joins

--- spec/channels/base_spec.rb
+++ spec/channels/base_spec.rb
@@ -19,4 +19,26 @@ describe ChatChannel, type: :channel do
 
     expect(subscription).to_not have_stream_for(room)
   end
+
+  it 'broadcasts users joining' do
+    stub_connection current_user: user
+
+    expect{subscribe room: room.name}
+      .to have_broadcasted_to(room)
+      .from_channel(ChatChannel)
+      .once
+      .with{ |data|
+        message = JSON.parse(data[:message], symbolize_names: true)
+        expect(message[:type]).to eq("user-joined")
+
+        payload = message[:data]
+        expected = {
+          name: user.name,
+          current: [
+            user.name
+          ]
+        }
+        expect(payload).to eq(expected)
+      }
+  end
 end
